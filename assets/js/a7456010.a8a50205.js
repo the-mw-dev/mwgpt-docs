2b951c155f3600578dc47e7733c5f02e8cb053ad
mw_dev <79410241+the-mw-dev@users.noreply.github.com>
2024-11-01 21:29:05 +0300

    Delete assets directory

--- assets/js/a7456010.a8a50205.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkip_6_lat_docs=self.webpackChunkip_6_lat_docs||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- assets/js/aba21aa0.dd7c180a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkip_6_lat_docs=self.webpackChunkip_6_lat_docs||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
