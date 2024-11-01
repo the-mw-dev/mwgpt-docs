14cb15989c0f46062ecc76e0150e093c6b7f5bc9
mw_dev <79410241+the-mw-dev@users.noreply.github.com>
2024-11-01 21:27:17 +0300

    Add files via upload

---
+++ assets/js/a7456010.a8a50205.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkip_6_lat_docs=self.webpackChunkip_6_lat_docs||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/aba21aa0.dd7c180a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkip_6_lat_docs=self.webpackChunkip_6_lat_docs||[]).push([[742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
